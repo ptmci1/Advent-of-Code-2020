5a4e18a6d89ff270f2ae8da23c60a5e35f94fbd7
Paul McIntosh <ptmci1@student.monash.edu>
2021-01-22 18:09:31 +1100

    Resolved issues with read_file caused by legacy code

--- Puzzle2.py
+++ Puzzle2.py
@@ -1,10 +1,19 @@
+# AoC 2020 Puzzle 2
+
 
 # Reading input from text file, converting to array of ints
 def read_file():
-    input_raw = open("Input1.txt", "r")
+    input_raw = open("Input2.txt", "r")
     input_arr = input_raw.read().split('\n')
-    input_arr = [int(elem) for elem in input_arr]
     input_raw.close()
     return input_arr
 
+
 def solve(input_arr):
+    for elem in input_arr:
+        # low_bound = elem.split()
+        low_bound = list(elem.split()[0])
+        return low_bound
+
+
+print(solve(read_file()))
