2d93cee579a898137fa2d6c3fa78b10a6a5f998a
Paul McIntosh <ptmci1@student.monash.edu>
2021-01-22 19:20:12 +1100

    Puzzle 2 both parts complete

--- Puzzle2.py
+++ Puzzle2.py
@@ -12,16 +12,27 @@ def read_file():
 def solve1(input_arr):
     acc = 0
     for elem in input_arr:
-        low_bound = elem.split('-')[0]
-        high_bound = elem.split()[0].split('-')[1]
+        low_bound = int(elem.split('-')[0])
+        high_bound = int(elem.split()[0].split('-')[1])
         target = list(elem.split()[1])[0]
         password = elem.split()[2]
-        if int(high_bound) >= password.count(target) >= int(low_bound):
+        if high_bound >= password.count(target) >= low_bound:
             acc += 1
     return acc
 
 
 def solve2(input_arr):
+    acc = 0
+    for elem in input_arr:
+        index1 = int(elem.split('-')[0]) - 1
+        index2 = int(elem.split()[0].split('-')[1]) - 1
+        target = list(elem.split()[1])[0]
+        password = list(elem.split()[2])
+        if password[index1] == target and password[index2] != target:
+            acc += 1
+        if password[index1] != target and password[index2] == target:
+            acc += 1
+    return acc
 
 
-# print(solve2(read_file()))
+print(solve2(read_file()))
