8e7fb74060c8397361e9abfa5bb5b136d27b538c
Paul McIntosh <ptmci1@student.monash.edu>
2021-01-22 21:08:07 +1100

    Puzzle 3 part two started. Solve1 now takes x increment and y increment parameters. Switch statement in solve1 needs refactoring to account for x_increment values other than 3.

--- Puzzle3.py
+++ Puzzle3.py
@@ -9,7 +9,8 @@ def read_file():
     return input_arr
 
 
-def solve1(input_arr):
+# TODO refactor switch statement, to account for x increments other than 3
+def solve1(input_arr, x_increment, y_increment):
     acc = 0
     x_axis = 0
     switch = {
@@ -18,11 +19,20 @@ def solve1(input_arr):
         30: -1
     }
     for elem in input_arr:
-        # print(x_axis)
-        if list(elem)[x_axis] == '#':
-            acc += 1
-        x_axis = switch.get(x_axis, x_axis) + 3
+        if y_increment % y_increment == 0:
+            if list(elem)[x_axis] == '#':
+                acc += 1
+        x_axis = switch.get(x_axis, x_axis) + x_increment
     return acc
 
 
-print(solve1(read_file()))
+def solve2(input_arr):
+    acc = solve1(input_arr, 1, 1)
+    acc *= solve1(input_arr, 3, 1)
+    acc *= solve1(input_arr, 5, 1)
+    acc *= solve1(input_arr, 7, 1)
+    acc *= solve1(input_arr, 1, 2)
+    return acc
+
+
+print(solve2(read_file()))
